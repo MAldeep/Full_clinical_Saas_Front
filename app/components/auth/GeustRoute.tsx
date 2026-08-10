4fc2859ddbdfcbe8bc67325c013e9961aff71e39
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-10 20:54:54 +0300

    client-side guard 2

---
+++ app/components/auth/GeustRoute.tsx
@@ -0,0 +1,24 @@
+import { useIsMounted } from "@/app/hooks/useIsMounted";
+import { useAuthStore } from "@/app/stores/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface GuestRouteProps {
+  children: React.ReactNode;
+}
+export default function GeustRoute({ children }: GuestRouteProps) {
+  const router = useRouter();
+  const isMounted = useIsMounted();
+  const { user, accessToken } = useAuthStore();
+  const isAuthenticated = Boolean(user && accessToken);
+
+  useEffect(() => {
+    if (isMounted && isAuthenticated) {
+      router.replace("/dashboard");
+    }
+  }, [isMounted, isAuthenticated, router]);
+  if (!isMounted || isAuthenticated) {
+    return null;
+  }
+  return <>{children}</>;
+}
